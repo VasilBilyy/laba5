62c3ad1dfecde2749323d7a164baa2c0d8a139b8
VasilBilyy <bilyjvasil@gmail.com>
2023-12-28 17:26:32 +0200

    рішення першого завдання

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,37 @@
 package org.example;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.*;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+
+
 public class Main {
+
+    static boolean check_number(String number){
+        return number.matches("\\(\\+380\\)\\d{2}-\\d{3}-\\d{2}-\\d{2}");
+    }
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        List<String> phoneNumbers = Arrays.asList("(+380)50-333-33-33", "(+380)67-5234-225-55", "123-45-678"); // додайте номери для перевірки
+
+        for (String phoneNumber : phoneNumbers) {
+            if (check_number(phoneNumber)) {
+                System.out.println("Номер телефону " + phoneNumber + " вірний");
+            } else {
+                System.out.println("Номер телефону " + phoneNumber + " невірний");
+            }
+        }
+
+        // Приклад 2:
+        String inputEmails = "john@example.com, alice@gmail.com, bob@yahoo.com";
+        Pattern pattern2 = Pattern.compile("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}");
+        Matcher matcher2 = pattern2.matcher(inputEmails);
+
+        while (matcher2.find()) {
+            System.out.println("Знайдено email: " + matcher2.group());
         }
     }
 }
